c54e8c0200ba0e357a5ec0ab1c0eb7697082390a
MelonHell <MelonHell@ya.ru>
2024-09-04 12:24:31 +0300

    allow any char in unquoted string

--- src/main/java/com/mojang/brigadier/StringReader.java
+++ src/main/java/com/mojang/brigadier/StringReader.java
@@ -167,11 +167,7 @@ public class StringReader implements ImmutableStringReader {
     }
 
     public static boolean isAllowedInUnquotedString(final char c) {
-        return c >= '0' && c <= '9'
-            || c >= 'A' && c <= 'Z'
-            || c >= 'a' && c <= 'z'
-            || c == '_' || c == '-'
-            || c == '.' || c == '+';
+        return c != ' ';
     }
 
     public String readUnquotedString() {
